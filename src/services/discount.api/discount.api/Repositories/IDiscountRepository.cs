42d085f761675be66b9fc3c3f4a522a37bbed183
William A Stewart <william.a.stewart@outlook.com>
2022-02-10 11:24:56 -0500

    Phase 1/2 of discount api complete

---
+++ src/services/discount.api/discount.api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using discount.api.Entities;
+
+namespace discount.api.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
