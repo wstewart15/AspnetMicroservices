a192c35d382fbb22e0e10696cabebf567644eea5
William A Stewart <william.a.stewart@outlook.com>
2022-02-11 11:32:54 -0500

    Completed discount.grpc

---
+++ src/services/discount/discount.grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using discount.grpc.Entities;
+using discount.grpc.protos;
+
+namespace discount.grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
--- src/services/discount/discount.grpc/Program.cs
+++ src/services/discount/discount.grpc/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddGrpc();
 
 var app = builder.Build();
@@ -17,7 +18,7 @@ var app = builder.Build();
 app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
