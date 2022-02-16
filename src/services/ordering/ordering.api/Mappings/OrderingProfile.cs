99386c82605a2ce1d1effe0399072852f67b3348
William A Stewart <william.a.stewart@outlook.com>
2022-02-16 12:45:02 -0500

    Completed event consumption algorithm.

---
+++ src/services/ordering/ordering.api/Mappings/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using ordering.application.Features.Orders.Commands.CheckoutOrder;
+
+namespace ordering.api.Mappings
+{
+	public class OrderingProfile : Profile
+	{
+		public OrderingProfile()
+		{
+			CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+		}
+	}
+}
--- src/services/ordering/ordering.api/Program.cs
+++ src/services/ordering/ordering.api/Program.cs
@@ -1,3 +1,6 @@
+using EventBus.Messages.Common;
+using MassTransit;
+using ordering.api.EventBusConsumers;
 using ordering.api.Extensions;
 using ordering.application;
 using ordering.infrastructure;
@@ -8,6 +11,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+//Configure MassTransit MessageBus
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
+
+// General Configuration
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
